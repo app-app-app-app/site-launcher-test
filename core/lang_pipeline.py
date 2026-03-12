5b338b41c90e6a20998f3111d3d3cb193c4ea24a
app-app-app-app <lobanskijila@gmail.com>
2026-03-12 23:18:14 +0200

    Update lang_pipeline.py

--- core/lang_pipeline.py
+++ core/lang_pipeline.py
@@ -4,6 +4,7 @@ import re
 import json
 import random
 from typing import Dict, List, Optional, Callable, Tuple
+from core.options import geo
 
 
 # OpenAI (required)
@@ -1223,7 +1224,7 @@ def _llm_transform_strings_onepass(
         protected_list.append(ps)
         maps.append(mp)
 
-    country_name = geo_defaults[cc]["name"]
+    country_name = geo[cc]["name"]
     
     system = (
         "You are processing a list of website phrases. "
