0d7b9849e68b382cdbacec1d53a107ce714fca97
app-app-app-app <lobanskijila@gmail.com>
2026-04-01 12:17:57 +0300

    Update sheets.py

--- core/sheets.py
+++ core/sheets.py
@@ -1,16 +1,2 @@
-import streamlit as st
-
 def add_to_google_sheet(data: dict):
-"""
-data = {
-"brand": "...",
-"geo": "...",
-"lang": "...",
-"domains": [...]
-}
-"""
-
-```
-# ПОКИ ПРОСТО ЛОГ
-print("Saving to Google Sheets:", data)
-```
+print("TEST:", data)
