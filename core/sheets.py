a43db0dfc877e417f0501ca8933361e4acd9664c
app-app-app-app <lobanskijila@gmail.com>
2026-04-01 12:00:44 +0300

    Create sheets.py

---
+++ core/sheets.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def add_to_google_sheet(data: dict):
+"""
+data = {
+"brand": "...",
+"geo": "...",
+"lang": "...",
+"domains": [...]
+}
+"""
+
+```
+# ПОКИ ПРОСТО ЛОГ
+print("Saving to Google Sheets:", data)
+```
